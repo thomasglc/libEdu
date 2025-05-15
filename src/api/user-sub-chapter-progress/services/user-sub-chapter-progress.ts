fb184ce32c8ad25d082d1b5313624320da765839
Thomas <thomas.galocha@hotmail.fr>
2025-05-15 20:22:18 +0200

    wip try some custom, let see

--- src/api/user-sub-chapter-progress/services/user-sub-chapter-progress.ts
+++ src/api/user-sub-chapter-progress/services/user-sub-chapter-progress.ts
@@ -4,4 +4,24 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::user-sub-chapter-progress.user-sub-chapter-progress');
+export default factories.createCoreService('api::user-sub-chapter-progress.user-sub-chapter-progress', ({ strapi }) => ({
+
+  async find(...args) {
+    const query = args[0];
+
+    query.filters = {
+      ...query.filters,
+      documentId: 'k18uo8kdc0ayqu1kx7vur6x5',
+    };
+    const { results, pagination } = await super.find(...args);
+
+    // some custom logic
+    results.forEach(result => {
+      result.counter = 1;
+      result.args = args;
+      result.query = query;
+    });
+
+    return { results, pagination };
+  },
+}));
